2da56054322b59391e4a8da31a226471bcdddf83
KimSoYeonnn <10sosokim10@gmail.com>
2025-04-11 10:43:00 +0900

    todo list config

---
+++ app/routes/about.tsx
@@ -0,0 +1,13 @@
+
+
+function AboutPage() {
+
+    return (
+        <div>
+            <div className={'text-4xl'}>AboutPage</div>
+            
+        </div>
+    )
+}
+
+export default AboutPage 
