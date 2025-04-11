1d80a30fece7b125b4654d5646d5303559a1f581
KimSoYeonnn <10sosokim10@gmail.com>
2025-04-11 11:15:40 +0900

    todo add config

--- app/routes.ts
+++ app/routes.ts
@@ -4,7 +4,8 @@ export default [
     index("routes/home.tsx"),
     route('/about', 'routes/about.tsx'),
     route("/todo", "layout/todoLayout.tsx", [
-        route("list",'routes/todo/listPage.tsx')
+        route("list",'routes/todo/listPage.tsx'),
+        route("add", 'routes/todo/addPage.tsx')
     ])
 
 ] satisfies RouteConfig;
