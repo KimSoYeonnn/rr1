a770c1a9683c5b875c9a5dcc146c5ef8a2153ccd
KimSoYeonnn <10sosokim10@gmail.com>
2025-04-15 09:27:34 +0900

    isFetching

--- app/components/todo/listComponent.tsx
+++ app/components/todo/listComponent.tsx
@@ -1,10 +1,33 @@
+import {useQuery} from "@tanstack/react-query";
+import {testTodoList} from "~/api/todoAPI";
+
 
 function TodoListComponent () {
+
+    // Queries
+    const query = useQuery({
+        queryKey: ['todos'],
+        queryFn: async() => {
+
+
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            const todos:Todo[] = await testTodoList()
+
+            return todos
+        }
+    })
+
+    const {isFetching, data, error } = query
+
     return (
         <div>
-            <div className={'text-4xl'}>Todo List Component</div>
+            <div className={'text-4xl'}>Todo List Component </div>
+            <div className={'text-3xl bg-amber-600'}> {isFetching && <h1>Loading.........</h1>}</div>
+
+            {data && <div className={'text-2xl'}>List 출력</div>}
         </div>
     )
 }
 
-export default TodoListComponent;
+export default TodoListComponent
---
+++ app/types/todo.d.ts
@@ -0,0 +1,8 @@
+
+interface Todo {
+    "tno": number,
+    "title": string,
+    "writer": string,
+    "regDate"?: Date | null,
+    "modDate"?: Date | null
+}
